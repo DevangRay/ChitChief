d46b0670072c2a258981f3c422ce5728849d255e
Devang Ray <devangray624@gmail.com>
2026-04-01 22:31:25 -0400

    set-up worker service

---
+++ api/src/worker.ts
@@ -0,0 +1,53 @@
+import { Worker } from 'bullmq';
+import { createPrismaClient } from './lib/prisma-factory';
+import IORedis from 'ioredis';
+import { SeatStatus } from '@prisma/client';
+import { seatLockKeyFormatter } from './lib/redis-keys';
+import 'dotenv/config';
+
+console.log('[worker] Starting worker with environment variables:', process.env.DATABASE_URL, process.env.REDIS_URL);
+const connection = new IORedis(
+    process.env.REDIS_URL!,
+    {
+        maxRetriesPerRequest: null, // Required for BullMQ
+    });
+const prisma = createPrismaClient(process.env.DATABASE_URL!);
+
+const worker = new Worker(
+    'reservations',
+    async (job) => {
+        if (job.name === 'expire_seat_reservation') {
+            console.log('[worker] Processing expire_seat_reservation job with data:', job.data);
+            const { seat_ids } = job.data;
+
+            // set seat_status back to AVAILABLE for all seat_ids in job data
+            await prisma.seat.updateMany({
+                where: {
+                    id: {
+                        in: seat_ids
+                    },
+                    seat_status: SeatStatus.RESERVED
+                },
+                data: {
+                    seat_status: 'AVAILABLE'
+                }
+            })
+
+            // delete redis locks for all seat_ids in job data
+            const lock_keys = seat_ids.map((id: string) => seatLockKeyFormatter(id));
+            // using pipeline instead of multi since we don't need atomicity. Will increase performance, and expiry is already set for keys
+            let multi_chain = connection.pipeline();
+            for (const key of lock_keys) {
+                multi_chain = multi_chain.del(key);
+            }
+            await multi_chain.exec();
+
+            console.log(`[worker] Released locks and reset seat statuses for seat ids: ${seat_ids.join(', ')}`);
+        }
+    },
+    { connection }
+);
+
+worker.on('failed', (job, err) => {
+    console.error(`[worker] Job ${job?.id} failed with error: ${err.message}. Full error:`, err);
+})
