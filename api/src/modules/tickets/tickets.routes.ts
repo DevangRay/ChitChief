83e3e870427f5d06f35dfd616045a5bb1e87f8cf
Devang Ray <devangray624@gmail.com>
2026-04-02 23:26:03 -0400

    added retry jitter to deal with h2h conflict over same tickets

--- api/src/modules/tickets/tickets.routes.ts
+++ api/src/modules/tickets/tickets.routes.ts
@@ -4,6 +4,8 @@ import { Seat, SeatStatus } from "@prisma/client";
 import SeatConflictError from "../../lib/custom_errors/SeatConflictError";
 import ResourceNotFoundError from "../../lib/custom_errors/ResourceNotFoundError";
 
+// MAX_RETRIES > 0 (otherwise /reserve will auto return 500)
+const MAX_RETRIES = 3;
 export default async function routes(fastify: FastifyInstance, options: Object) {
     const service = new TicketService(fastify.redis, fastify.prisma);
 
@@ -29,25 +31,50 @@ export default async function routes(fastify: FastifyInstance, options: Object) 
             ]
 
             const seat_ids = seats_array.map(seat => seat.id);
+            const user_id = '2d62c96e-0078-4653-98ba-595644b67b82';
 
             // add retry jitter -> catch case where multiple requests come in at same time for same seats, and all get through redis lock check before locks are set
-            const result = await service.reserveSeats(seat_ids, '2d62c96e-0078-4653-98ba-595644b67b82');
-            console.dir(result)
-            return reply.status(200).send(result);
+            let attempt_error: Error | undefined;
+            for (let attempt = 0; attempt < MAX_RETRIES; attempt++) {
+                try {
+                    console.log(`Running attempt #${attempt}`);
+                    const result = await service.reserveSeats(seat_ids, user_id);
 
-        } catch (error) {
-            console.log("error: " + error)
+                    return reply.status(200).send(result);
+                } catch (error) {
+                    attempt_error = error as Error;
 
-            if (error instanceof SeatConflictError) {
-                console.log("[tickets.routes]: Caught 409 Error")
-                return reply.status(409).send({ message: error.message, conflict_seat_ids: error.conflict_seat_ids });
-            } else if (error instanceof ResourceNotFoundError) {
-                console.log("[tickets.routes]: Caught 404 Error")
-                return reply.status(404).send({ message: error.message });
-            } else {
-                fastify.log.error(error, '[GET /reserve] Failed to reserve seats');
-                return reply.status(500).send({ message: 'Internal server error.' });
+                    if (attempt_error instanceof SeatConflictError) {
+                        console.log("[tickets.routes]: Caught 409 Error. Retrying after random time.")
+
+                        // avoids pointlessly sleeping before return
+                        if (attempt < MAX_RETRIES - 1) {
+                            // random_time is in 100s of milliseconds
+                            const random_time = 50 + (Math.random() * 100);
+                            console.log("random_time is:", random_time);
+                            await new Promise(r => setTimeout(r, random_time));
+                        }
+
+                        continue;
+                    } else if (attempt_error instanceof ResourceNotFoundError) {
+                        console.log("[tickets.routes]: Caught 404 Error. Returning.")
+                        // ResourceNotFoundError will not be fixed in retry. Should be returned immediately.
+                        return reply.status(404).send({ message: attempt_error.message });
+                    }
+                }
             }
+
+            if (attempt_error instanceof SeatConflictError) {
+                console.log("[tickets.routes]: Caught 409 Error")
+                return reply.status(409).send({ message: attempt_error.message, conflict_seat_ids: attempt_error.conflict_seat_ids });
+            }
+            
+            // Unexpcted error after retries
+            return reply.status(500).send({ message: 'Internal server error.' });
+        } catch (error) {
+            console.log("error: " + error);
+            fastify.log.error(error, '[GET /reserve] Failed to reserve seats');
+            return reply.status(500).send({ message: 'Internal server error.' });
         }
     })
 }
